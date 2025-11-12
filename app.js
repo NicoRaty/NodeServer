cac8caad1538b6f3f32e69222103f02e6dc9e0e9
NicoRaty <nicoraty02@gmail.com>
2025-11-12 20:47:43 +0200

    Added OpenAPI documentation support with Swagger.

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 import express from 'express'
 import http from 'http'
 
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
+
 import homeRouter from './routes/home.js'
 import dataRouter from './routes/data.js'
 import indexRouter from './routes/index.js'
 
 const app = express()
+const swaggerDocument = YAML.load('./openapi.yaml')
 
 const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use('/', homeRouter)
 app.use('/data', dataRouter)
